39c2d2714be31e2701d1d483fa346e2755db6c0b
tom rottier <tomrottier@outlook.com>
2024-05-21 22:14:59 -0400

    zsh/install working

--- zsh/install.sh
+++ zsh/install.sh
@@ -9,14 +9,15 @@ fi
 # install oh my zsh
 if [ ! -d "${HOME}/.oh-my-zsh" ]; then
 	echo installing oh-my-zsh
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
+	RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 # install plugins
 if [ ! -d  "${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions" ]; then
-	echo installing zsh-completions
-	git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+	echo installing zsh-suggestions
+	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 fi
+
 if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting" ]; then
 	echo installing zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
@@ -29,5 +30,8 @@ if [ ! -d "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k" ]; then
 fi
 
 # overwrite config files
-mv dotfiles/zsh/.zshrc $HOME
-mv dotfiles/zsh/.p10k.zsh $HOME
+cp dotfiles/zsh/.zshrc $HOME
+cp dotfiles/zsh/.p10k.zsh $HOME
+
+source .zshrc
+zsh
