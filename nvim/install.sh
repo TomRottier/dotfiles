b1fd8ebb0cd2d293dbe750846b5130d6f528e24e
tom <tomrottier@outlook.com>
2024-06-17 12:05:57 -0400

    install latest version of nvim

--- nvim/install.sh
+++ nvim/install.sh
@@ -1,4 +1,6 @@
 if [ ! -f /opt/nvim-linux64/bin/nvim ]; then
 	echo -e "\e[36minstalling neovim\e[0m"
-	apt install neovim
+	curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux64.tar.gz
+	sudo rm -rf /opt/nvim
+	sudo tar -C /opt -xzf nvim-linux64.tar.gz
 fi
